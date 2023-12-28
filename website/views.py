2da5525d4cbe77da323cd836c191c2779e252212
César Júnior <cesar.jnr@hotmail.com>
2023-12-28 20:17:50 -0300

    Add login functionality

--- website/views.py
+++ website/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
   return render(request, 'home.html', {})
 
 def login_user(request):
-  return render(request, 'login.html', {})
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+
+      return redirect('home')
+    else:
+      messages.error(request, 'Invalid credentials')
+
+      return redirect('login')
+  else:
+    return render(request, 'login.html', {})
