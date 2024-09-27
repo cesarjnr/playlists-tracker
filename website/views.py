6a28a9f1474cdf00a3e0bfea9e4a785f79358e7f
César Júnior <cesarjunior@MacBook-Pro-de-Cesar.local>
2024-09-27 17:02:29 -0300

    Implement user registration

--- website/views.py
+++ website/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from .forms import SignUpForm
+
 def home(request):
-  return render(request, 'home.html', {})
+  if request.user.is_authenticated:
+    return render(request, 'home.html', {})
+  else:
+    return redirect('login')
 
 def login_user(request):
-  if (request.user.is_authenticated):
+  if request.user.is_authenticated:
     return redirect('home')
   else:
     if request.method == 'POST':
@@ -29,3 +34,18 @@ def logout_user(request):
   logout(request)
 
   return redirect('login')
+
+def register_user(request):
+  if request.method == 'POST':
+    form = SignUpForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+
+      return redirect('login')
+    else:
+      return render(request, 'register.html', {'form': form})
+  else:
+    form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form})
